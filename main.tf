883290a71305b1366d8da26d377a13f04512a9f3
binayapuri <binaya.puri@apexcollege.edu.np>
2023-12-11 23:15:47 +0545

    aws cloudfront configured

--- main.tf
+++ main.tf
@@ -69,25 +69,60 @@ data "aws_iam_policy_document" "website_policy" {
   }
 }
 
-#Attaching the policy to the bucket
-# resource "aws_s3_bucket_policy" "s3_ppt_policy" {
-#   bucket = aws_s3_bucket.s3_ppt.id
-#   policy = data.aws_iam_policy_document.website_policy.json
-# }
 
 
-resource "aws_s3_bucket_policy" "s3_ppt_policy" {
-  bucket = aws_s3_bucket.s3_ppt.id
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "PublicReadGetObject",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "s3:GetObject",
-        "Resource": "arn:aws:s3:::www.hamrotech.com.np/*"
-      }
-    ]
-  })
+##################################################################################
+##########################  CLoud Front  ########################################
+##################################################################################
+
+
+resource "aws_cloudfront_distribution" "static_website" {
+ origin {
+  domain_name = "www.hamrotech.com.np.s3-website.us-east-2.amazonaws.com"
+  origin_id = "S3Origin"
+
+  custom_origin_config {
+    http_port            = 80
+    https_port           = 443
+    origin_protocol_policy = "http-only"
+    origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+  }
+ }
+
+ enabled           = true
+ is_ipv6_enabled   = true
+ default_root_object = "index.html"
+
+ default_cache_behavior {
+  allowed_methods = ["GET", "HEAD"]
+  cached_methods = ["GET", "HEAD"]
+  target_origin_id = "S3Origin"
+
+  forwarded_values {
+    query_string = false
+
+    cookies {
+      forward = "none"
+    }
+  }
+
+  viewer_protocol_policy = "allow-all"
+  min_ttl              = 0
+  default_ttl          = 3600
+  max_ttl              = 86400
+ }
+
+ viewer_certificate {
+  cloudfront_default_certificate = true
+ }
+
+ restrictions {
+  geo_restriction {
+    restriction_type = "none"
+  }
+ }
+
+ tags = {
+  Environment = "production"
+ }
 }
